481c89033c8b31ceb1ae47a37a73b180104957a7
yadimaksta <90519111+yadimaksta@users.noreply.github.com>
2022-03-18 22:08:01 +0300

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -1,95 +1,74 @@
-var canvas, ctx,
-    brush = {
-        x: 0,
-        y: 0,
-        color: '#000000',
-        size: 10,
-        down: false,
-    },
-    strokes = [],
-    currentStroke = null;
 
+
+
+
+// РИСОВАЛКА
+let
+  canvas, ctx,
+  brush = {
+    x: 0,
+    y: 0,
+    color: '#FD4E2B',
+    size: 8,
+    down: false,
+  },
+  strokes = [],
+  currentStroke = null;
 function redraw () {
-    ctx.clearRect(0, 0, canvas.width(), canvas.height());
-    ctx.lineCap = 'round';
-    for (var i = 0; i < strokes.length; i++) {
-        var s = strokes[i];
-        ctx.strokeStyle = s.color;
-        ctx.lineWidth = s.size;
-        ctx.beginPath();
-        ctx.moveTo(s.points[0].x, s.points[0].y);
-        for (var j = 0; j < s.points.length; j++) {
-            var p = s.points[j];
-            ctx.lineTo(p.x, p.y);
-        }
-        ctx.stroke();
+  ctx.clearRect(0, 0, canvas.width(), canvas.height());
+  ctx.lineCap = 'round';
+  for (let i = 0; i < strokes.length; i++) {
+    let s = strokes[i];
+    ctx.strokeStyle = s.color;
+    ctx.lineWidth = s.size;
+    ctx.beginPath();
+    ctx.moveTo(s.points[0].x, s.points[0].y);
+    for (let j = 0; j < s.points.length; j++) {
+      let p = s.points[j];
+      ctx.lineTo(p.x, p.y);
     }
+    ctx.stroke();
+  }
 }
-
 function init () {
-    canvas = $('#draw');
-    canvas.attr({
-        width: window.innerWidth,
-        height: window.innerHeight,
+  canvas = $('#draw');
+  canvas.attr({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  ctx = canvas[0].getContext('2d');
+  function mouseEvent (e) {
+    brush.x = e.pageX;
+    brush.y = e.pageY;
+    currentStroke.points.push({
+      x: brush.x,
+      y: brush.y,
     });
-    ctx = canvas[0].getContext('2d');
-
-    function mouseEvent (e) {
-        brush.x = e.pageX;
-        brush.y = e.pageY;
-
-        currentStroke.points.push({
-            x: brush.x,
-            y: brush.y,
-        });
-
-        redraw();
-    }
-
-    canvas.mousedown(function (e) {
-        brush.down = true;
-
-        currentStroke = {
-            color: brush.color,
-            size: brush.size,
-            points: [],
-        };
-
-        strokes.push(currentStroke);
-
-        mouseEvent(e);
-    }).mouseup(function (e) {
-        brush.down = false;
-
-        mouseEvent(e);
-
-        currentStroke = null;
-    }).mousemove(function (e) {
-        if (brush.down)
-            mouseEvent(e);
-    });
-
-    $('#save-btn').click(function () {
-        window.open(canvas[0].toDataURL());
-    });
-
-    $('#undo-btn').click(function () {
-        strokes.pop();
-        redraw();
-    });
-
-    $('#clear-btn').click(function () {
-        strokes = [];
-        redraw();
-    });
-
-    $('#color-picker').on('input', function () {
-        brush.color = this.value;
-    });
-
-    $('#brush-size').on('input', function () {
-        brush.size = this.value;
-    });
+    redraw();
+  }
+  canvas.mousedown(function (e) {
+    brush.down = true;
+    currentStroke = {
+      color: brush.color,
+      size: brush.size,
+      points: [],
+    };
+    strokes.push(currentStroke);
+    mouseEvent(e);
+  }).mouseup(function (e) {
+      brush.down = false;
+      mouseEvent(e);
+    currentStroke = null;
+  }).mousemove(function (e) {
+    if (brush.down)
+    mouseEvent(e);
+  });
+  $('.clearButton').click(function () {
+    strokes = [];
+    redraw();
+  });
+  $('#color-picker').on('input', function () {
+      brush.color = this.value;
+  });
 }
-
 $(init);
