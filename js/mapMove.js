2fb424d9dbb70d276af124599129d88933781cea
yadimaksta <90519111+yadimaksta@users.noreply.github.com>
2022-03-18 12:54:21 +0300

    Update mapMove.js

--- js/mapMove.js
+++ js/mapMove.js
@@ -1,10 +1,7 @@
 let container = document.querySelector('.container');
 let canvas = document.querySelector('.canvas');
-
 container.addEventListener('mousemove', (e) => {
-
   let x = e.clientX - container.getBoundingClientRect().left;
   let y = e.clientY - container.getBoundingClientRect().top;
-
-  canvas.style.transform = `translate(-${x*2}px, -${y*2}px)`;
+  canvas.style.transform = `translate(-${x*2}px, -${y*2.4}px)`;
 })
