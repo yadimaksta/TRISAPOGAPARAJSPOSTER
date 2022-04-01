b1a5f3bafa632fbe11985da4ffe14ced02b94761
yadimaksta <90519111+yadimaksta@users.noreply.github.com>
2022-04-01 11:51:12 +0300

    Update cursor.js

--- js/cursor.js
+++ js/cursor.js
@@ -14,94 +14,71 @@ $(document).mousemove(function(e){
   }, 20)
 })
 
-// КЛИК
 $(document).ready(function(){
+  // КЛИК
   $(".CLICKELEMENTS").hover(function(){
     $(".cursorLabel").addClass("cursorLabelClick");
   },function(){
     $(".cursorLabel").removeClass("cursorLabelClick");
   });
-});
-$(document).ready(function(){
   $(".CLICKELEMENTS").hover(function(){
     $(".cursor").addClass("cursorClick");
   },function(){
     $(".cursor").removeClass("cursorClick");
   });
-});
-
-$(document).ready(function(){
   $(".CLICKELEMENTSDISABLE").hover(function(){
     $(".cursorLabel").addClass("cursorLabelClickDis");
   },function(){
     $(".cursorLabel").removeClass("cursorLabelClickDis");
   });
-});
-$(document).ready(function(){
   $(".CLICKELEMENTSDISABLE").hover(function(){
     $(".cursor").addClass("cursorClicks");
   },function(){
     $(".cursor").removeClass("cursorClick");
   });
-});
 
-// РИСОВАЛКА
-$(document).ready(function(){
+  // РИСОВАЛКА
   $("#draw").hover(function(){
     $(".cursorLabel").addClass("cursorLabelDraw");
   },function(){
     $(".cursorLabel").removeClass("cursorLabelDraw");
   });
-});
-$(document).ready(function(){
   $("#draw").hover(function(){
     $(".cursor").addClass("cursorDraw");
   },function(){
     $(".cursor").removeClass("cursorDraw");
   });
-});
 
-// ПЕРС
-$(document).ready(function(){
+  // ПЕРС
   $(".persBody").hover(function(){
     $(".cursorLabel").addClass("cursorLabelClickAndMove");
   },function(){
     $(".cursorLabel").removeClass("cursorLabelClickAndMove");
   });
-});
-$(document).ready(function(){
   $(".persBody").hover(function(){
     $(".cursor").addClass("cursorClick");
   },function(){
     $(".cursor").removeClass("cursorClick");
   });
-});
 
-// ДРАГ НАВЕДЕНИЕ
-$(document).ready(function(){
+  // ДРАГ НАВЕДЕНИЕ
   $(".shapesPath").hover(function(){
     $(".cursorLabel").addClass("cursorLabelDragAndClick");
   },function(){
     $(".cursorLabel").removeClass("cursorLabelDragAndClick");
   });
-});
-$(document).ready(function(){
   $(".shapesPath").hover(function(){
     $(".cursor").addClass("cursorClick");
   },function(){
     $(".cursor").removeClass("cursorClick");
   });
-});
 
-// ГРЯЗЬ НАВЕДЕНИЕ
-$(document).ready(function(){
+  // ГРЯЗЬ НАВЕДЕНИЕ
   $(".dirt").hover(function(){
     $(".cursorLabel").addClass("cursorLabelClick");
   },function(){
     $(".cursorLabel").removeClass("cursorLabelClick");
   });
-});
-$(document).ready(function(){
   $(".dirt").hover(function(){
     $(".cursor").addClass("cursorClick");
   },function(){
